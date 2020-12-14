82c5397a500cece007f5d18b9520e5f0e1ffcfe7
Peter Togara <petertogara@gmail.com>
2020-12-14 09:06:19 +0200

    Initial commit

---
+++ src/main/java/com/playsafe/roullette/RoulletteApplication.java
@@ -0,0 +1,13 @@
+package com.playsafe.roullette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoulletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoulletteApplication.class, args);
+    }
+
+}
